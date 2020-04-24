e2d9aa4c8ba69c7dabe19b04da8cee24e280ffca
jieun <love9ly@gmail.com>
2020-04-24 15:10:20 +0900

    make var

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,18 @@ var router = express.Router();
 let server = require('http');
 let svr, port = 12345;
 
+const id1 = "urn:covid-19:case:001";
+const id2 = "urn:covid-19:case:002";
+
 svr = server.createServer((req, res) => {
 	res.writeHead(200, { 'X-M2M-RSC': 2000 });
 
 	req.on('data', (chunk) => {
 		console.log(chunk.toString() + '\n');
 	}).on('end', () => {
-		res.end(JSON.stringify(
+		res.end(JSON.stringify([
 			{
-				"id": "urn:covid-19:case:seongnam:006",
+				"id": "urn:covid-19:case:001",
 				"type": "InfectionCase",
 				"createdAt": "2020-03-10T20:10:00,000+09:00",
 				"modifiedAt": "2020-03-10T20:10:00,000+09:00",
@@ -38,7 +41,19 @@ svr = server.createServer((req, res) => {
 				},
 				"diseaseCode": {
 					"type": "Property",
-					"value": "COVID-19"
+					"value": "COVID-19",
+					"definedBy": {
+						"type": "Property",
+						"value": "WHO",
+						"supportBy": {
+							"type": "Property",
+							"value": "jieun"
+						}
+					},
+					"xxx": {
+						"type": "Relationship",
+						"object": "urn:covid-19:case:002"
+					}
 				},
 				"infectinStatus": {
 					"type": "Property",
@@ -130,18 +145,151 @@ svr = server.createServer((req, res) => {
 						}
 					]
 				},
-				"spreader": [
-					{
+				"spreader": {
+					"type": "Relationship",
+					"object": "urn:covid-19:case:002",
+					"spreaderInfo": {
 						"type": "Property",
 						"value": {
-							"caseId": "urn:covid-19:case:00031",
-							"nickname": "대구003",
-							"relationship": "종교",
-							"description": "신천지"
+							"relationship": " ",
+							"description": " "
+						}
+					},
+					"yyy": {
+						"type": "Relationship",
+						"object": "urn:covid-19:case:002"
+					}
+				}
+				
+			},
+			{
+				"id": "urn:covid-19:case:002",
+				"type": "InfectionCase",
+				"createdAt": "2020-03-10T20:10:00,000+09:00",
+				"modifiedAt": "2020-03-10T20:10:00,000+09:00",
+				"nickname": {
+					"type": "Property",
+					"value": "성남6"
+				},
+				"yearOfBrith": {
+					"type": "Property",
+					"value": 1970
+				},
+				"gender": {
+					"type": "Property",
+					"value": "male"
+				},
+				"address": {
+					"type": "Property",
+					"value": {
+						"addressCountry": "KR",
+						"addressRegion": "경기도",
+						"addressLocality": "성남시"
+					}
+				},
+				"diseaseCode": {
+					"type": "Property",
+					"value": "COVID-19",
+					"definedBy": {
+						"type": "Property",
+						"value": "WHO",
+						"supportBy": {
+							"type": "Property",
+							"value": "jieun"
 						}
 					}
-				]
-			}
+				},
+				"infectinStatus": {
+					"type": "Property",
+					"value": "확진격리"
+				},
+				"levelOfSymptom": {
+					"type": "Property",
+					"value": "중증"
+				},
+				"orderOfTransmission": {
+					"type": "Property",
+					"value": 3
+				},
+				"detentionPlaceName": {
+					"type": "Property",
+					"value": "분당서울대병원"
+				},
+				"detentionAddress": {
+					"type": "Property",
+					"value": {
+						"addressCountry": "KR",
+						"addressRegion": "경기도",
+						"addressLocality": "성남시",
+						"streetAddress": "구미로173번길 82"
+					}
+				},
+				"detentionLocation": {
+					"type": "GeoProperty",
+					"value": {
+						"type": "Point",
+						"coordinates": [
+							127.123164,
+							37.351981
+						]
+					}
+				},
+				"travelRoutes": {
+					"type": "Property",
+					"value": [
+						{
+							"placeName": "회사",
+							"address": {
+								"type": "Property",
+								"value": {
+									"addressCountry": "KR",
+									"addressRegion": "경기도",
+									"addressLocality": "성남시",
+									"streetAddress": "성남대로 1111"
+								}
+							},
+							"location": {
+								"type": "GeoProperty",
+								"value": {
+									"type": "Point",
+									"coordinates": [
+										127.123164,
+										37.351981
+									]
+								}
+							},
+							"transport": "버스",
+							"beginTime": "2020-02-15T10:00:00,000+09:00",
+							"duration": 3600
+						},
+						{
+							"placeName": "카페2020",
+							"address": {
+								"type": "Property",
+								"value": {
+									"addressCountry": "KR",
+									"addressRegion": "경기도",
+									"addressLocality": "성남시",
+									"streetAddress": "성남대로 2222"
+								}
+							},
+							"location": {
+								"type": "GeoProperty",
+								"value": {
+									"type": "Point",
+									"coordinates": [
+										127.123154,
+										37.351911
+									]
+								}
+							},
+							"transport": "도보",
+							"beginTime": "2020-02-15T12:00:00,000+09:00",
+							"duration": 3600
+						}
+					]
+				}
+			}]
 		))
 	});
 
